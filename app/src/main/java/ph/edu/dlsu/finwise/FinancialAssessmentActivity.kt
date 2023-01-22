3a80efd19b158be80fcd78031937758316e639f2
sherick02 <karl_sherick_yu@dlsu.edu.ph>
2023-01-22 18:34:28 +0800

    Added some class and fixed some xml

--- app/src/main/java/ph/edu/dlsu/finwise/FinancialAssessmentActivity.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/FinancialAssessmentActivity.kt
@@ -1,4 +1,12 @@
 package ph.edu.dlsu.finwise
 
-class FinancialAssessmentActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FinancialAssessmentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_financial_assessment)
+    }
 }
--- app/src/main/java/ph/edu/dlsu/finwise/ProfileActivity.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/ProfileActivity.kt
@@ -1,4 +1,12 @@
 package ph.edu.dlsu.finwise
 
-class ProfileActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+
 }
