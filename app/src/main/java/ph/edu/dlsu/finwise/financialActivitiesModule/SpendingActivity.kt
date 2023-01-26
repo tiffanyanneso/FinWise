042c2b5a406a3a6ec7b08b2d91b6deb63393d9cf
misaeliana <69031743+misaeliana@users.noreply.github.com>
2023-01-26 15:15:00 +0800

    spending tabbed layout

--- app/src/main/java/ph/edu/dlsu/finwise/financialActivitiesModule/SpendingActivity.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/financialActivitiesModule/SpendingActivity.kt
@@ -2,11 +2,41 @@ package ph.edu.dlsu.finwise.financialActivitiesModule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ph.edu.dlsu.finwise.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import ph.edu.dlsu.finwise.*
+import ph.edu.dlsu.finwise.databinding.ActivityProfileBinding
+import ph.edu.dlsu.finwise.databinding.ActivitySpendingBinding
 
 class SpendingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySpendingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_spending)
+        binding = ActivitySpendingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+
+        adapter.addFragment(SpendingCategoriesFragment(),"Categories")
+        adapter.addFragment(SpendingHistoryFragment(),"History")
+
+        binding.viewPager.adapter = adapter
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+    }
+
+    class ViewPagerAdapter(fm : FragmentManager) : FragmentStatePagerAdapter(fm){
+
+        private val mFrgmentList = ArrayList<Fragment>()
+        private val mFrgmentTitleList = ArrayList<String>()
+        override fun getCount() = mFrgmentList.size
+        override fun getItem(position: Int) = mFrgmentList[position]
+        override fun getPageTitle(position: Int) = mFrgmentTitleList[position]
+
+        fun addFragment(fragment: Fragment, title:String){
+            mFrgmentList.add(fragment)
+            mFrgmentTitleList.add(title)
+        }
     }
 }
