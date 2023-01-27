ec6750947e10bf68a859b615947993bfbcae7270
sherick02 <karl_sherick_yu@dlsu.edu.ph>
2023-01-27 17:36:23 +0800

    adjustments

--- app/src/main/java/ph/edu/dlsu/finwise/personalFinancialManagementModule/TransactionDetailsActivity.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/personalFinancialManagementModule/TransactionDetailsActivity.kt
@@ -1,12 +1,35 @@
 package ph.edu.dlsu.finwise.personalFinancialManagementModule
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import ph.edu.dlsu.finwise.R
+import ph.edu.dlsu.finwise.databinding.ActivityPfmtransactionDetailsBinding
 
 class TransactionDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPfmtransactionDetailsBinding
+    var bundle = Bundle()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pfmtransaction_details)
+        binding = ActivityPfmtransactionDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener{
+
+        }
+        goToBack()
+
     }
+
+    private fun goToBack() {
+        binding.btnBack.setOnClickListener{
+
+            val goBack= Intent(this, PersonalFinancialManagementActivity::class.java)
+            goBack.putExtras(bundle)
+            startActivity(goBack)
+        }
+    }
+
 }
