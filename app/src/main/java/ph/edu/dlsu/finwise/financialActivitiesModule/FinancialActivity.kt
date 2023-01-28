af9fd13b40dd706babec3f6ec5512ec219d84ba6
MiguelG619 <69783877+MiguelG619@users.noreply.github.com>
2023-01-28 20:07:28 +0800

    Added sort in financialActivity

--- app/src/main/java/ph/edu/dlsu/finwise/financialActivitiesModule/FinancialActivity.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/financialActivitiesModule/FinancialActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -16,6 +18,9 @@ class FinancialActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFinancialBinding
     private lateinit var goalAdapter: ChildGoalAdapter
+    private var goalIDArrayList = ArrayList<String>()
+    private lateinit var status: String
+
 
     private var firestore = Firebase.firestore
 
@@ -28,35 +33,77 @@ class FinancialActivity : AppCompatActivity() {
 
         // Hides actionbar,
         // and initializes the navbar
-       supportActionBar?.hide()
-       Navbar(findViewById(R.id.bottom_nav), this, R.id.nav_goal)
+        supportActionBar?.hide()
+        Navbar(findViewById(R.id.bottom_nav), this, R.id.nav_goal)
+        goToNewGoal()
+        getAllGoals()
+        sortTransactions()
+    }
+    private fun sortTransactions() {
+        val sortSpinner = binding.spinnerStatus
+        sortSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long
+            ) {
+                status = sortSpinner.selectedItem.toString()
 
-        getGoals()
+                if (status == "--Status--") {
+                    goalIDArrayList.clear()
+                    getAllGoals()
+                }
+                else {
+                    goalIDArrayList.clear()
+                    firestore.collection("FinancialGoals")
+                        .whereEqualTo("status", status).get().addOnSuccessListener { documents ->
+                            for (goalSnapshot in documents) {
+                                //creating the object from list retrieved in db
+                                val goalID = goalSnapshot.id
+                                goalIDArrayList.add(goalID)
+                            }
+                            loadRecyclerView(goalIDArrayList)
+                        }
+                }
+            }
 
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                // your code here
+            }
+        }
+    }
+
+    private fun getAllGoals() {
+
+        //TODO:change to get transactions of current user
+        //var currentUser = FirebaseAuth.getInstance().currentUser!!.uid
+        //firestore.collection("Transactions").whereEqualTo("companyID", currentUser).get().addOnSuccessListener{ documents ->
+
+        firestore.collection("FinancialGoals").get().addOnSuccessListener { documents ->
+            for (goalSnapshot in documents) {
+                //creating the object from list retrieved in db
+                val goalID = goalSnapshot.id
+                goalIDArrayList.add(goalID)
+            }
+            loadRecyclerView(goalIDArrayList)
+        }
+    }
+
+    private fun loadRecyclerView(goalIDArrayList: ArrayList<String>) {
+        goalAdapter = ChildGoalAdapter(this, goalIDArrayList)
+        binding.rvViewGoals.adapter = goalAdapter
+        binding.rvViewGoals.layoutManager = LinearLayoutManager(applicationContext,
+            LinearLayoutManager.VERTICAL,
+            false)
+        goalAdapter.notifyDataSetChanged()
+    }
+
+    private fun goToNewGoal() {
         binding.btnNewGoal.setOnClickListener {
-            context=this
-            var goToNewGoal = Intent(context, ChildNewGoal::class.java)
+            val goToNewGoal = Intent(this, ChildNewGoal::class.java)
             context.startActivity(goToNewGoal)
         }
     }
 
-    private fun getGoals() {
-        var goalIDArrayList = ArrayList<String>()
-
-        //TODO:change to get goals of current user
-        //var currentUser = FirebaseAuth.getInstance().currentUser!!.uid
-        //firestore.collection("FinancialGoals").whereEqualTo("childID", currentUser).get().addOnSuccessListener{ documents ->
-
-        firestore.collection("FinancialGoals").get().addOnSuccessListener{ documents ->
-            for (goalSnapshot in documents) {
-                //creating the object from list retrieved in db
-                val goalID = goalSnapshot.id
-                goalIDArrayList.add(goalID!!)
-            }
-
-            binding.rvViewGoals.setLayoutManager(LinearLayoutManager(applicationContext))
-            goalAdapter = ChildGoalAdapter(applicationContext, goalIDArrayList)
-            binding.rvViewGoals.setAdapter(goalAdapter)
-        }
-    }
 }
