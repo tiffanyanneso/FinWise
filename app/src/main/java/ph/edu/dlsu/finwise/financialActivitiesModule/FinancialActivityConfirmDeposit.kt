ffd8d7c4c544bf6eb1c5cc14f2327a958e5c3809
MiguelG619 <69783877+MiguelG619@users.noreply.github.com>
2023-01-28 11:25:15 +0800

    Populated spinners in PFM module

--- app/src/main/java/ph/edu/dlsu/finwise/financialActivitiesModule/FinancialActivityConfirmDeposit.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/financialActivitiesModule/FinancialActivityConfirmDeposit.kt
@@ -49,9 +49,9 @@ class FinancialActivityConfirmDeposit : AppCompatActivity() {
                 "decisionMakingActivityID" to decisionActivityID
             )
             firestore.collection("Transactions").add(transaction).addOnSuccessListener {
-                var bundle = Bundle()
+                val bundle = Bundle()
                 bundle.putString("decisionMakingActivityID", decisionActivityID)
-                var viewDeposits = Intent(this, SavingActivity::class.java)
+                val viewDeposits = Intent(this, SavingActivity::class.java)
                 viewDeposits.putExtras(bundle)
                 viewDeposits.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 context.startActivity(viewDeposits)
--- app/src/main/java/ph/edu/dlsu/finwise/personalFinancialManagementModule/RecordIncomeActivity.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/personalFinancialManagementModule/RecordIncomeActivity.kt
@@ -1,9 +1,15 @@
 package ph.edu.dlsu.finwise.personalFinancialManagementModule
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import ph.edu.dlsu.finwise.databinding.ActivityPfmrecordIncomeBinding
+import ph.edu.dlsu.finwise.model.FinancialGoals
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -11,13 +17,30 @@ class RecordIncomeActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityPfmrecordIncomeBinding
     var bundle = Bundle()
+    private var firestore = Firebase.firestore
+    private var goals = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPfmrecordIncomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getGoals()
         goToConfirmation()
         cancel()
+    }
+
+    private fun getGoals() {
+        //TODO: UPDATE LATER WITH CHILD ID
+        goals.add("None")
+        firestore.collection("FinancialGoals").whereEqualTo("status", "In Progress").get().addOnSuccessListener { results ->
+            for (goal in results) {
+                var goalObject = goal.toObject<FinancialGoals>()
+                goals.add(goalObject.goalName.toString())
+            }
+            val adapter = ArrayAdapter(this, R.layout.simple_list_item_1, goals)
+            binding.spinnerGoal.adapter = adapter
+        }
     }
 
     private fun cancel() {
