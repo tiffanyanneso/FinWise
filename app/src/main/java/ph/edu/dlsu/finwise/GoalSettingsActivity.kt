29d5d792d7c6e0300489e4418bfb4fe16177aced
misaeliana <69031743+misaeliana@users.noreply.github.com>
2023-01-28 21:12:00 +0800

    goal settings ui

---
+++ app/src/main/java/ph/edu/dlsu/finwise/GoalSettingsActivity.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.finwise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoalSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_settings)
+    }
+}
