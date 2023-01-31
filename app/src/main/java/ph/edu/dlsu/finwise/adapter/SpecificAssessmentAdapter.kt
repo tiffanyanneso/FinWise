88c1ce423882b12a2c3b717364d91c8d61bb5e5b
Sherick Yu <karl_sherick_yu@dlsu.edu.ph>
2023-02-01 03:19:07 +0800

    added finlit expert specific assessment adapter and fragments

---
+++ app/src/main/java/ph/edu/dlsu/finwise/adapter/SpecificAssessmentAdapter.kt
@@ -0,0 +1,23 @@
+package ph.edu.dlsu.finwise.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import ph.edu.dlsu.finwise.fragment.DetailsFragment
+import ph.edu.dlsu.finwise.fragment.QuestionsFragment
+
+class SpecificAssessmentAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter (fragmentManager, lifecycle)
+{
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position){
+            0 -> return DetailsFragment()
+            1 -> return QuestionsFragment()
+            else -> return DetailsFragment()
+        }
+    }
+}
--- app/src/main/java/ph/edu/dlsu/finwise/financialAssessmentModuleFinlitExpert/FinlitExpertSpecificAssessmentActivity.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/financialAssessmentModuleFinlitExpert/FinlitExpertSpecificAssessmentActivity.kt
@@ -2,14 +2,30 @@ package ph.edu.dlsu.finwise.financialAssessmentModuleFinlitExpert
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import ph.edu.dlsu.finwise.NavbarFinlitExpert
 import ph.edu.dlsu.finwise.R
+import ph.edu.dlsu.finwise.adapter.SpecificAssessmentAdapter
 
 class FinlitExpertSpecificAssessmentActivity : AppCompatActivity() {
+
+    var tabTitle = arrayOf("Details", "Questions")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_financial_assessment_finlit_expert_specific_assessment)
+
+        var pager = findViewById<ViewPager2>(R.id.viewPager2)
+        var tl = findViewById<TabLayout>(R.id.tabLayout)
+        pager.adapter = SpecificAssessmentAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(tl, pager){
+            tab, position ->
+                tab.text = tabTitle[position]
+        } .attach()
+
 
         // Hides actionbar,
         // and initializes the navbar
