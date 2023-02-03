8820048b91857188106b2ea0c6e942a0351a48b0
misaeliana <69031743+misaeliana@users.noreply.github.com>
2023-02-03 16:27:12 +0800

    parent goals w partial fragments
    - will fix ui
    - will make other fragments for other statuses later

---
+++ app/src/main/java/ph/edu/dlsu/finwise/ParentGoalActivity.kt
@@ -0,0 +1,55 @@
+package ph.edu.dlsu.finwise
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import ph.edu.dlsu.finwise.databinding.ActivityFinancialBinding
+import ph.edu.dlsu.finwise.databinding.ActivityParentGoalBinding
+import ph.edu.dlsu.finwise.financialActivitiesModule.FinancialActivity
+
+class ParentGoalActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityParentGoalBinding
+    private var firestore = Firebase.firestore
+    private lateinit var context: Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityParentGoalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        context = this
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        //checkSettings()
+
+        // TODO: change the fragments added based on parent approval
+        adapter.addFragment(ParentInProgressFragment(),"In Progress")
+//        adapter.addFragment(GoalForReviewFragment(),"For Review")
+//        adapter.addFragment(GoalForEditingFragment(),"For Editing")
+//        adapter.addFragment(GoalDisapprovedFragment(),"Disapproved")
+        adapter.addFragment(ParentAchievedFragment(),"Achieved")
+
+        binding.viewPager.adapter = adapter
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+
+    }
+
+    class ViewPagerAdapter(fm : FragmentManager) : FragmentStatePagerAdapter(fm){
+
+        private val mFrgmentList = ArrayList<Fragment>()
+        private val mFrgmentTitleList = ArrayList<String>()
+        override fun getCount() = mFrgmentList.size
+        override fun getItem(position: Int) = mFrgmentList[position]
+        override fun getPageTitle(position: Int) = mFrgmentTitleList[position]
+
+        fun addFragment(fragment: Fragment, title:String){
+            mFrgmentList.add(fragment)
+            mFrgmentTitleList.add(title)
+        }
+    }
+}
