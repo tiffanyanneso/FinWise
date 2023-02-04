24c7eeb45bb3b1fe138bf710f074561572d4a760
misaeliana <69031743+misaeliana@users.noreply.github.com>
2023-02-04 23:52:15 +0800

    maya payment ui

---
+++ app/src/main/java/ph/edu/dlsu/finwise/MayaPayment.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.finwise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MayaPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maya_payment)
+    }
+}
