e7e7110b2e3ad8d889bf65db54203e6e991a9fe5
misaeliana <69031743+misaeliana@users.noreply.github.com>
2023-02-08 22:58:24 +0800

    updated date ui

--- app/src/main/java/ph/edu/dlsu/finwise/EditProfileActivity.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/EditProfileActivity.kt
@@ -1,10 +1,14 @@
 package ph.edu.dlsu.finwise
 
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
@@ -12,6 +16,8 @@ import com.google.firebase.ktx.Firebase
 import ph.edu.dlsu.finwise.databinding.ActivityEditProfileBinding
 import ph.edu.dlsu.finwise.databinding.ActivityProfileBinding
 import ph.edu.dlsu.finwise.model.ChildUser
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditProfileActivity : AppCompatActivity() {
 
@@ -19,6 +25,9 @@ class EditProfileActivity : AppCompatActivity() {
     private var firestore = Firebase.firestore
     private lateinit var context: Context
 
+    lateinit var birthday: Date
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
@@ -26,6 +35,10 @@ class EditProfileActivity : AppCompatActivity() {
         context= this
 
         getProfileData()
+
+        binding.etBirthday.setOnClickListener{
+            showCalendar()
+        }
 
         binding.btnSave.setOnClickListener{
             updateInternProfile()
@@ -80,5 +93,23 @@ class EditProfileActivity : AppCompatActivity() {
         }.addOnFailureListener{
             Toast.makeText(this, "Profile Update Failed", Toast.LENGTH_SHORT)
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun showCalendar() {
+        val dialog = Dialog(this)
+
+        dialog.setContentView(ph.edu.dlsu.finwise.R.layout.dialog_calendar)
+        dialog.window!!.setLayout(1000, 1200)
+
+        var calendar = dialog.findViewById<DatePicker>(ph.edu.dlsu.finwise.R.id.et_date)
+
+        calendar.setOnDateChangedListener { datePicker: DatePicker, mYear, mMonth, mDay ->
+            binding.etBirthday.setText((mMonth + 1).toString() + "/" + mDay.toString() + "/" + mYear.toString())
+            birthday = SimpleDateFormat("MM/dd/yyyy").parse((mMonth + 1).toString() + "/" +
+                    mDay.toString() + "/" + mYear.toString())
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
