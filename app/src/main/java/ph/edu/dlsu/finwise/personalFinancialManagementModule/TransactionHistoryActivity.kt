22d8acab251193585ff367e345004d4897c180c0
sherick02 <karl_sherick_yu@dlsu.edu.ph>
2023-02-14 22:45:27 +0800

    added contents on transaction filter
    - added front end for transaction filter
    - adjusted some ui

--- app/src/main/java/ph/edu/dlsu/finwise/personalFinancialManagementModule/TransactionHistoryActivity.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/personalFinancialManagementModule/TransactionHistoryActivity.kt
@@ -93,8 +93,8 @@ class TransactionHistoryActivity : AppCompatActivity() {
     }
 
     private fun loadBackButton() {
-        binding.topA33333ppBar.navigationIcon = ResourcesCompat.getDrawable(resources, ph.edu.dlsu.finwise.R.drawable.baseline_arrow_back_24, null)
-        binding.topA33333ppBar.setNavigationOnClickListener {
+        binding.topAppBar.navigationIcon = ResourcesCompat.getDrawable(resources, ph.edu.dlsu.finwise.R.drawable.baseline_arrow_back_24, null)
+        binding.topAppBar.setNavigationOnClickListener {
             val goToPFM = Intent(applicationContext, PersonalFinancialManagementActivity::class.java)
             startActivity(goToPFM)
         }
