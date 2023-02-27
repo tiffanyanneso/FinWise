b02b60e49af91487638b224ca27715e1bd92c624
misaeliana <69031743+misaeliana@users.noreply.github.com>
2023-02-27 22:30:10 +0800

    remaining parent involvement screens
    will connect fragments to activities tom

--- app/src/main/java/ph/edu/dlsu/finwise/BudgetExpenseFragment.kt
+++ app/src/main/java/ph/edu/dlsu/finwise/BudgetExpenseFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [EarningCompletedFragment.newInstance] factory method to
+ * Use the [BudgetExpenseFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class EarningCompletedFragment : Fragment() {
+class BudgetExpenseFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class EarningCompletedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_earning_completed, container, false)
+        return inflater.inflate(R.layout.fragment_budget_expense, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class EarningCompletedFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment EarningCompletedFragment.
+         * @return A new instance of fragment BudgetExpenseFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            EarningCompletedFragment().apply {
+            BudgetExpenseFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
---
+++ app/src/main/java/ph/edu/dlsu/finwise/SpendingTransactionsActivity.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.finwise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpendingTransactionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spending_transactions)
+    }
+}
