217ab87fa704e696d5af780705102044df3fb065
tiffanyanneso <tiffany_anne_so@dlsu.edu.ph>
2023-02-27 13:21:58 +0800

    simple ui for leaderboard

---
+++ app/src/main/java/ph/edu/dlsu/finwise/LeaderboardActivity.kt
@@ -0,0 +1,71 @@
+package ph.edu.dlsu.finwise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import ph.edu.dlsu.finwise.adapter.LeaderboardRankingAdapter
+import ph.edu.dlsu.finwise.databinding.ActivityLeaderboardBinding
+import ph.edu.dlsu.finwise.model.AssessmentAttempts
+import ph.edu.dlsu.finwise.model.Friends
+
+class LeaderboardActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityLeaderboardBinding
+    private var friendsUserIDArrayList = ArrayList<String>()
+    private var friendRankingArrayList = ArrayList<Ranking>()
+
+    private var firestore = Firebase.firestore
+
+    private lateinit var leaderboardRankingAdapter: LeaderboardRankingAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLeaderboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // and initializes the navbar
+        supportActionBar?.hide()
+        Navbar(findViewById(R.id.bottom_nav), this, R.id.nav_finance)
+
+        getFriends()
+    }
+
+    private fun getFriends() {
+        var currentUser = "eWZNOIb9qEf8kVNdvdRzKt4AYrA2"
+        firestore.collection("Friends").whereEqualTo("senderID", currentUser).whereEqualTo("status", "Accepted").get().addOnSuccessListener { results ->
+            for (friend in results) {
+                var request = friend.toObject<Friends>()
+                friendsUserIDArrayList.add(request.receiverID.toString())
+            }
+        }.continueWith {
+            firestore.collection("Friends").whereEqualTo("receiverID", currentUser).whereEqualTo("status", "Accepted").get().addOnSuccessListener { results ->
+                for (friend in results) {
+                    var request = friend.toObject<Friends>()
+                    friendsUserIDArrayList.add(request.senderID.toString())
+                }
+            }
+        }.continueWith { getScores() }
+    }
+
+    private fun getScores() {
+        for (friendUserID in friendsUserIDArrayList) {
+            var totalCorrect = 0
+            var totalQuestions = 0
+            firestore.collection("AssessmentAttempts").whereEqualTo("childID", friendUserID).get().addOnSuccessListener { results ->
+                for (assessmentAttempt in results) {
+                    var assessmentAttemptObject = assessmentAttempt.toObject<AssessmentAttempts>()
+                    totalCorrect += assessmentAttemptObject?.nAnsweredCorrectly!!
+                    totalQuestions += assessmentAttemptObject?.nQuestions!!
+                }
+            }.continueWith {
+                friendRankingArrayList.add(Ranking(0, friendUserID, totalCorrect.toFloat().div(totalQuestions.toFloat())))
+                friendRankingArrayList.sortBy { it.score }
+                println("print " + friendRankingArrayList)
+            }
+        }
+    }
+    class Ranking (var rank:Int, var childID:String, var score:Float)
+}
